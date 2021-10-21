8cb93b9a5cfe1b0d40e450c118ee20760fde2397
Vlad Kravets <vl4d.kravets@gmail.com>
2021-10-21 11:25:03 -0400

    Init commit

---
+++ src/main/java/com/demo/emc/EmcApplication.java
@@ -0,0 +1,13 @@
+package com.demo.emc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmcApplication.class, args);
+	}
+
+}
